a87dfce0a2c57b4e4f47e30c7a625025d4f719db
zsofip <91950899+zsofip@users.noreply.github.com>
2021-11-13 19:49:43 +0100

    finally done

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,19 @@
+'use strict';
 
+//Írj egy függvénykifejezést, amely a paraméterként megadott tömbből eltávolítja a duplikált elemeket!
+// A függvény visszatérési értéke a duplikált elemektől mentes új tömb.
 
-
-
+const removeDuplicatesFromArray = (anyArray = []) => {
+    let noDupArray = [];
+    
+    for (let value of anyArray) {
+        if(noDupArray.indexOf(value) === -1) {
+            noDupArray.push(value);
+        }
+    }
+    return noDupArray;
+} 
 
 
 // export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray;
